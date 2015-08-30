194211ea5a988fb3635cec2bce1ead2f382e1a76
bonsa <kasia.wasak@gmail.com>
2015-08-30 21:57:31 +0200

    Added service which fetchs day descripton from outside API

--- src/main/java/angularspringapp/controller/DayController.java
+++ src/main/java/angularspringapp/controller/DayController.java
@@ -1,10 +1,13 @@
 package angularspringapp.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import angularspringapp.service.ApiService;
 
 @Controller
 @RequestMapping("/day")
@@ -12,8 +15,13 @@ public class DayController {
 
 	static final Logger log = Logger.getLogger(DayController.class);
 
+	@Autowired
+	private ApiService apiService;
+	
 	@RequestMapping("/{day}/{month}")
 	public @ResponseBody String getDayDescription(@PathVariable("day") String day, @PathVariable("month") String month) {
+		
+		apiService.getFactAboutDay(day, month);
 		
 		log.info("Day: " + day + " + Month: " + month);
 
