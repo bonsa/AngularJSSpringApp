c4d069db5d65dd95dca76d9b669b0d453d94fafd
bonsa <kasia.wasak@gmail.com>
2015-08-30 15:46:47 +0200

    display default description about day

--- src/main/java/angularspringapp/DayController.java
+++ src/main/java/angularspringapp/DayController.java
@@ -1,11 +1,23 @@
 package angularspringapp;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/day")
 public class DayController {
+
+	static final Logger log = Logger.getLogger(DayController.class);
+
+	@RequestMapping("/{day}/{month}")
+	public @ResponseBody String getDayDescription(@PathVariable("day") String day, @PathVariable("month") String month) {
+		log.info("Day: " + day + " + Month: " + month);
+
+		return "Ellen Fairclough is sworn in as Canada's first woman Cabinet Minister, year 1957";
+	}
 
 	@RequestMapping("/layout")
 	public String getDayPartialPage() {
--- src/main/webapp/resources/js/controllers/DayController.js
+++ src/main/webapp/resources/js/controllers/DayController.js
@@ -1,14 +1,22 @@
 'use strict';
 
 var DayController = function($scope, $http) {
-	 $scope.dayInYear = {
-		        day: '1',
-		        pattern: /^([1-9]|1[0-9]|2[0-9]|3[0-1])$/
-		      };
-	 
-	 $scope.monthInYear = {
-		        month: '3',
-		        pattern: /^([1-9]|1[0-2])$/
-		      };
+	$scope.dayOfYear = {
+		day : '1',
+		pattern : /^([1-9]|1[0-9]|2[0-9]|3[0-1])$/
+	};
+
+	$scope.monthOfYear = {
+		month : '3',
+		pattern : /^([1-9]|1[0-2])$/
+	};
+
+	$scope.getDayDescription = function() {
+		$http
+		.get('day/' + $scope.dayOfYear.day + '/' + $scope.monthOfYear.month)
+		.success(function(description) {
+			$scope.description = description;
+		});
+	};
 
 };
