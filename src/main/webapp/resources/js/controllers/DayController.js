4acbbef0e6d0f01e33239ce16d76a2ba69965fed
bonsa <kasia.wasak@gmail.com>
2015-08-30 15:12:59 +0200

    added fields to day page

--- src/main/webapp/resources/js/controllers/DayController.js
+++ src/main/webapp/resources/js/controllers/DayController.js
@@ -1,5 +1,14 @@
 'use strict';
 
 var DayController = function($scope, $http) {
+	 $scope.dayInYear = {
+		        day: '1',
+		        pattern: /^([1-9]|1[0-9]|2[0-9]|3[0-1])$/
+		      };
+	 
+	 $scope.monthInYear = {
+		        month: '3',
+		        pattern: /^([1-9]|1[0-2])$/
+		      };
 
 };
