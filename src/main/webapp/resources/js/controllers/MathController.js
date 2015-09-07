86729f5bfcbef2dad1a7221de31bac0f6f624111
bonsa <kasia.wasak@gmail.com>
2015-09-07 23:17:25 +0200

    init math page, not completed

--- src/main/webapp/resources/js/controllers/MathController.js
+++ src/main/webapp/resources/js/controllers/MathController.js
@@ -3,4 +3,17 @@
 angular.module('AngularJSSpringApp')
 .controller('MathController',[ '$scope', '$http', function($scope, $http) {
 
+	$scope.number = {
+			value : '1000',
+			pattern : /^\[0-9]{4}$/
+		};
+	
+	$scope.getFactAboutNumber = function() {
+		$http
+		.get('math/' + $scope.number.value)
+		.success(function(description) {
+			$scope.description = description;
+		});
+	};
+	
 }]);
