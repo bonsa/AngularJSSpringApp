4e49a43fcffa82cf9d01368a5cbb94c96b75e8fc
bonsa <kasia.wasak@gmail.com>
2015-09-10 22:39:18 +0200

    sprint boot init - not working

---
+++ src/main/java/angularspringapp/configuration/ServiceApplication.java
@@ -0,0 +1,31 @@
+package angularspringapp.configuration;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class ServiceApplication
+{
+    public static void main(final String[] args)
+    {
+//        SpringApplication.run(ServiceApplication.class, args);
+        
+        ApplicationContext ctx = SpringApplication.run(ServiceApplication.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
+
--- src/main/java/angularspringapp/controller/DayController.java
+++ src/main/java/angularspringapp/controller/DayController.java
@@ -2,15 +2,15 @@ package angularspringapp.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import angularspringapp.model.DayResponse;
 import angularspringapp.service.ApiService;
 
-@Controller
+@RestController
 @RequestMapping("/day")
 public class DayController {
 
@@ -32,3 +32,4 @@ public class DayController {
 		return "/day/layout";
 	}
 }
+        
